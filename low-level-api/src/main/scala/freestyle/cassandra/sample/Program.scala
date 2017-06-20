8860cc42e839a6b7542a1152a631a026ac2597d9
Fede Fernández <federico.f.b@47deg.com>
2017-06-20 14:37:44 +0200

    Removes unnecessary imports

--- low-level-api/src/main/scala/freestyle/cassandra/sample/Program.scala
+++ low-level-api/src/main/scala/freestyle/cassandra/sample/Program.scala
@@ -20,11 +20,6 @@ import java.util.UUID
 
 import cats.instances.future._
 import freestyle._
-import freestyle.async._
-import freestyle.async.implicits._
-import freestyle.asyncMonix.implicits._
-import freestyle.implicits._
-import freestyle.cassandra.codecs._
 import freestyle.cassandra.sample.Implicits._
 import freestyle.cassandra.sample.Model._
 import freestyle.cassandra.api.LowLevelAPI
